f70c66174129a3c6a0ccda6422dc05c02e3367d6
Anastasiia Mironova <mironchik.1804@gmail.com>
2025-12-27 17:41:12 +0300

    Добавлена сортировка в двух направлениях

--- main.py
+++ main.py
@@ -4,9 +4,12 @@ arr = []
 for i in range(n):
     arr.append(int(input()))
 
+order = input("Введите направление сортировки (asc/desc): ")
+
 for i in range(n):
     for j in range(0, n - i - 1):
-        if arr[j] > arr[j + 1]:
+        if (order == "asc" and arr[j] > arr[j + 1]) or \
+           (order == "desc" and arr[j] < arr[j + 1]):
             arr[j], arr[j + 1] = arr[j + 1], arr[j]
 
-print("Отсортированный массив:", arr)
+print("Результат сортировки:", arr)
