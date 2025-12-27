603f129c29a859fd6506d1882ec40776ad181dc0
Anastasiia Mironova <mironchik.1804@gmail.com>
2025-12-27 17:00:08 +0300

    Сортировка пузырьком по возрастанию

---
+++ main.py
@@ -0,0 +1,12 @@
+n = int(input("Введите количество чисел и числа: "))
+arr = []
+
+for i in range(n):
+    arr.append(int(input()))
+
+for i in range(n):
+    for j in range(0, n - i - 1):
+        if arr[j] > arr[j + 1]:
+            arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+print("Отсортированный массив:", arr)
